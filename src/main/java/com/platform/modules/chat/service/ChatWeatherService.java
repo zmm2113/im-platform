e44e95d0b45b35d28a1fc3ca313f714eb9c7be31
q3z3 <939313737@qq.com>
2022-08-19 10:18:02 +0800

    优化天气查询

--- src/main/java/com/platform/modules/chat/service/ChatWeatherService.java
+++ src/main/java/com/platform/modules/chat/service/ChatWeatherService.java
@@ -1,6 +1,5 @@
 package com.platform.modules.chat.service;
 
-import cn.hutool.core.lang.Dict;
 import cn.hutool.json.JSONObject;
 
 import java.util.List;
@@ -14,17 +13,8 @@ import java.util.List;
 public interface ChatWeatherService {
 
     /**
-     * 实况天气
-     */
-    Dict queryBase(String cityCode);
-
-    /**
-     * 预报天气
-     */
-    Dict queryPredict(String cityCode);
-
-    /**
      * 预报天气
      */
     List<JSONObject> queryByCityName(String cityName);
+
 }
